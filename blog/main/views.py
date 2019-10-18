e15da85d5214f16218c018dc6ab999b7f9b008dd
kelly150 <poopypwner385@gmail.com>
2019-10-18 16:52:55 +0800

    add:main

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.http import HttpResponse
+
+
+def main(request):
+    '''
+    Show 'Hello world!' in the main page
+    '''
+    return HttpResponse('Hello world!')
+
