3253b8f802c7504c45e40577493caf2fed3cc77b
kelly150 <poopypwner385@gmail.com>
2019-10-25 16:02:56 +0800

    add:templates

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
 
 
-from django.http import HttpResponse
-
 
 def main(request):
     '''
     Render the main page
     '''
-    html = '''
-    <!doctype html>
-    <html>
-    <head>
-    <title>部落格</title>
-    <meta charset="utf-8">
-    </head>
-    <body>
-    <p>這是 HTML 版的 Hello world!</p>
-    </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    context = {'like':'Django 很棒'}
+    return render(request, 'main/main.html', context)
