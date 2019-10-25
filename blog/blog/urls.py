f652bf9f67350c72173c9db16073f4298476df29
kelly150 <poopypwner385@gmail.com>
2019-10-25 14:58:03 +0800

    add:urls

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from main import views
-
+ 
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('main/', include('main.urls', namespace='main')),
