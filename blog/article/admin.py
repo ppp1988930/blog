5be172cda2036e177c5f1e9d30ce501f23756151
kelly150 <poopypwner385@gmail.com>
2019-11-22 16:26:07 +0800

    ch8

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-
 from article.models import Article, Comment
 
 
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = ['article', 'content', 'pubDateTime']
+    list_display_links = ['article']
+    list_filter = ['article', 'content']
+    search_fields = ['content']
+    list_editable = ['content']
+ 
+    class Meta:
+        model = Comment
+        
+        
 admin.site.register(Article)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentModelAdmin)
